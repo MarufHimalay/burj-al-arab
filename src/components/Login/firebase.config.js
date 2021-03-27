7e182e41a9c7932b4d7d2aab02d3df97b7468a37
MarufHimalay <m.u.maruf1999@gmail.com>
2021-03-28 00:52:29 +0600

    secured node api jwt

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyADdm80fBCQGT6IKeAhtCQ6WS7T2qRHy2k",
+    authDomain: "burj-al-arab-himalay.firebaseapp.com",
+    projectId: "burj-al-arab-himalay",
+    storageBucket: "burj-al-arab-himalay.appspot.com",
+    messagingSenderId: "1079210620209",
+    appId: "1:1079210620209:web:0ed0c1c347578e5c84ec10"
+  };
+
+  export default firebaseConfig;
